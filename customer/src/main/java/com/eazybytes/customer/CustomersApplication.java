df84c7c10deaea0f5af1421e707a982810b9a022
Faycal Bououza <faycal.bououza@ratp.fr>
2025-02-12 16:56:28 +0100

    add dispatcher interceptor for validation purpose

--- customer/src/main/java/com/eazybytes/customer/CustomersApplication.java
+++ customer/src/main/java/com/eazybytes/customer/CustomersApplication.java
@@ -1,7 +1,11 @@
 package com.eazybytes.customer;
 
+import com.eazybytes.customer.command.interceptor.CustomerCommandInterceptor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +14,12 @@ public class CustomersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomersApplication.class, args);
+    }
+
+    @Autowired
+    public void registerCustomerCommandInterceptor(ApplicationContext context,
+                                                   CommandGateway commandGateway){
+        commandGateway.registerDispatchInterceptor(context.getBean(CustomerCommandInterceptor.class));
     }
 
 }
--- customer/src/main/java/com/eazybytes/customer/command/interceptor/CustomerCommandInterceptor.java
+++ customer/src/main/java/com/eazybytes/customer/command/interceptor/CustomerCommandInterceptor.java
@@ -1,4 +1,58 @@
 package com.eazybytes.customer.command.interceptor;
 
-public class CustomerCommandInterceptor {
+import com.eazybytes.customer.command.CreateCustomerCommand;
+import com.eazybytes.customer.command.DeleteCustomerCommand;
+import com.eazybytes.customer.command.UpdateCustomerCommand;
+import com.eazybytes.customer.entity.Customer;
+import com.eazybytes.customer.exception.CustomerAlreadyExistsException;
+import com.eazybytes.customer.exception.ResourceNotFoundException;
+import com.eazybytes.customer.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.messaging.MessageDispatchInterceptor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+@Component
+@RequiredArgsConstructor
+public class CustomerCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
+
+    private final CustomerRepository customerRepository;
+
+    @Nonnull
+    @Override
+    public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(@Nonnull List<? extends CommandMessage<?>> messages) {
+        return (index, command) -> {
+            if (CreateCustomerCommand.class.equals(command.getPayload().getClass())) {
+                CreateCustomerCommand createCustomerCommand = (CreateCustomerCommand) command.getPayload();
+                Optional<Customer> optionalCustomer = customerRepository.
+                        findByMobileNumberAndActiveSw(createCustomerCommand.getMobileNumber(), true);
+
+                if (optionalCustomer.isPresent()) {
+                    throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                            + createCustomerCommand.getMobileNumber());
+                }
+            } else if (UpdateCustomerCommand.class.equals(command.getPayload().getClass())) {
+                UpdateCustomerCommand updateCustomerCommand = (UpdateCustomerCommand) command.getPayload();
+                customerRepository.
+                        findByMobileNumberAndActiveSw(updateCustomerCommand.getMobileNumber(), true)
+                        .orElseThrow(() -> new ResourceNotFoundException("Customer", "mobileNumber",
+                                updateCustomerCommand.getMobileNumber()));
+            }
+
+            else if (DeleteCustomerCommand.class.equals(command.getPayload().getClass())) {
+                DeleteCustomerCommand deleteCustomerCommand = (DeleteCustomerCommand) command.getPayload();
+                customerRepository.
+                        findByCustomerIdAndActiveSw(deleteCustomerCommand.getCustomerId(), true)
+                        .orElseThrow(() -> new ResourceNotFoundException("Customer", "Id",
+                                deleteCustomerCommand.getCustomerId()));
+            }
+
+            return command;
+        };
+    }
 }
