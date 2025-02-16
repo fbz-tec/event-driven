0743b4f6408d46bbfbcc0e8d7dc671f192a1fddd
Faycal <bz.faycal@gmail.com>
2025-02-16 11:20:35 +0100

    fix delete card command

--- cards/src/main/java/com/eazybytes/cards/command/DeleteCardCommand.java
+++ cards/src/main/java/com/eazybytes/cards/command/DeleteCardCommand.java
@@ -2,10 +2,13 @@ package com.eazybytes.cards.command;
 
 import lombok.Builder;
 import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
 @Builder
 public class DeleteCardCommand {
+
+    @TargetAggregateIdentifier
     private final Long cardNumber;
     private final boolean activeSw;
 }
